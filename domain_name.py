26408636d93235fdd1a3a5046cc6f59d833a9b0f
Lance Lai <37203010+lancelaii@users.noreply.github.com>
2020-02-08 17:45:14 +0800

    Delete domain_name.py

--- domain_name.py
+++
@@ -1,9 +0,0 @@
-from tld import get_fld
-
-def get_domain_name(url, quiet=False):
-    if not quiet:
-        print( "Getting domain name,'{}'.".format( url ) );
-    
-    return get_fld( url );
-
-#print(get_domain_name('https://www.google.com'))
